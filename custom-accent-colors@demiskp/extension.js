02cb7b8714caca1ce9618feb353e67aeffbbc224
demiskp <dk222email@proton.me>
2022-11-23 20:55:41 +0200

    Initial commit

---
+++ custom-accent-colors@demiskp/extension.js
@@ -0,0 +1,239 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/* exported init */
+
+const { Gio, GLib } = imports.gi;
+
+const Main = imports.ui.main;
+const ExtensionUtils = imports.misc.extensionUtils;
+
+const Me = ExtensionUtils.getCurrentExtension();
+const MeDir = Me.dir.get_path();
+const HomeDir = GLib.get_home_dir();
+
+
+class Extension {
+
+    constructor(uuid) {
+        this._uuid = uuid;
+
+        //ExtensionUtils.initTranslations();
+    }
+
+    enable() {
+        this._settings = ExtensionUtils.getSettings(
+            'org.gnome.shell.extensions.custom-accent-colors');
+        
+        let _accentColor = this._settings.get_string('accent-color');
+        
+        this._settings.connect('changed::accent-color', () => {
+            _accentColor = this._settings.get_string('accent-color');
+            update_gtk4_theming(_accentColor);
+            if (this._settings.get_boolean('theme-gtk3') == true) {
+                update_gtk3_theming(this._settings.get_boolean('theme-gtk3'), _accentColor);
+            }
+            if (this._settings.get_boolean('theme-shell') == true) {
+                update_shell_theming(this._settings.get_boolean('theme-shell'), _accentColor);
+            }
+        });
+        this._settings.connect('changed::theme-flatpak', () => {
+            update_flatpak_theming(this._settings.get_boolean('theme-flatpak'));
+        });
+        this._settings.connect('changed::theme-gtk3', () => {
+            update_gtk3_theming(this._settings.get_boolean('theme-gtk3'), _accentColor);
+        });
+        this._settings.connect('changed::theme-shell', () => {
+            if (this._settings.get_boolean('theme-shell') == true) {
+                create_file_dir(HomeDir + '/.local/share/themes/CustomAccentColors/gnome-shell');
+                copy_file_dir(MeDir + '/resources/' + _accentColor + '/gnome-shell/toggle-on.svg',
+                    HomeDir + '/.local/share/themes/CustomAccentColors/gnome-shell/toggle-on.svg');
+            }
+            update_shell_theming(this._settings.get_boolean('theme-shell'), _accentColor);
+        });
+
+        update_gtk4_theming(_accentColor);
+        if (this._settings.get_boolean('theme-gtk3') == true) {
+            update_flatpak_theming(this._settings.get_boolean('theme-flatpak'));
+        }
+        if (this._settings.get_boolean('theme-gtk3') == true) {
+            update_gtk3_theming(this._settings.get_boolean('theme-gtk3'), _accentColor);
+        }
+        if (this._settings.get_boolean('theme-shell') == true) {
+            create_file_dir(HomeDir + '/.local/share/themes/CustomAccentColors/gnome-shell');
+            copy_file_dir(MeDir + '/resources/' + _accentColor + '/gnome-shell/toggle-on.svg',
+                HomeDir + '/.local/share/themes/CustomAccentColors/gnome-shell/toggle-on.svg');
+            update_shell_theming(this._settings.get_boolean('theme-shell'), _accentColor);
+        }
+    }
+
+    disable() {
+        delete_file_dir(HomeDir + "/.config/gtk-4.0/gtk.css");
+        if (this._settings.get_boolean('theme-flatpak') == true) {
+            try {
+                GLib.spawn_command_line_async(
+                    'flatpak override --user --nofilesystem=xdg-config/gtk-3.0');
+            } catch (e) {
+                logError(e);
+            }
+            try {
+                GLib.spawn_command_line_async(
+                    'flatpak override --user --nofilesystem=xdg-config/gtk-4.0');
+            } catch(e) {
+                logError(e);
+            }
+        }
+        if (this._settings.get_boolean('theme-gtk3') == true) {
+            delete_file_dir(HomeDir + '/.config/gtk-3.0/gtk.css');
+        }
+        if (this._settings.get_boolean('theme-shell') == true) {
+            delete_file_dir(HomeDir +
+                '/.local/share/themes/CustomAccentColors/gnome-shell/toggle-on.svg');
+            delete_file_dir(HomeDir +
+                '/.local/share/themes/CustomAccentColors/gnome-shell/gnome-shell.css');
+            delete_file_dir(HomeDir +
+                '/.local/share/themes/CustomAccentColors/gnome-shell');
+            delete_file_dir(HomeDir + '/.local/share/themes/CustomAccentColors');
+        }
+        
+        this._settings?.run_dispose();
+        this._settings = null;
+    }
+}
+
+function init() {
+    return new Extension();
+}
+
+function create_file_dir(path) {
+    const file = Gio.File.new_for_path(path);
+    try {
+        file.make_directory_with_parents(null);
+    } catch(e) {
+        log(e);
+    }
+}
+
+function read_file(path) {
+    const file = Gio.File.new_for_path(path);
+    const [, contents, etag] = file.load_contents(null);
+    const decoder = new TextDecoder('utf-8');
+    const contentsString = decoder.decode(contents);
+
+    return contentsString;
+}
+
+function write_file(str, path) {
+    const file = Gio.File.new_for_path(path);
+    const [, etag] = file.replace_contents(str, null, false,
+        Gio.FileCreateFlags.REPLACE_DESTINATION, null);
+}
+
+function copy_file_dir(path_source, path_target) {
+    const source = Gio.File.new_for_path(path_source);
+    const target = Gio.File.new_for_path(path_target);
+    try {
+        source.copy(target, Gio.FileCopyFlags.NONE, null, null);
+    } catch(e) {
+        log(e);
+    }
+}
+
+async function delete_file_dir(path) {
+    const file = Gio.File.new_for_path(path);
+    try {
+        await new Promise((resolve, reject) => {
+            file.delete_async(
+                GLib.PRIORITY_DEFAULT,
+                null,
+                (file_, result) => {
+                    try {
+                        resolve(file.delete_finish(result));
+                    } catch (e) {
+                        reject(e);
+                    }
+                }
+            );
+        });
+    } catch(e) {
+        log(e);
+    }
+}
+
+function update_gtk4_theming(accentcolor) {
+
+    const theme = read_file(MeDir + '/resources/' + accentcolor + '/gtk.css');
+    write_file(theme, HomeDir + '/.config/gtk-4.0/gtk.css');
+}
+
+
+function update_flatpak_theming(themeit) {
+    if (themeit == true) {
+        try {
+            GLib.spawn_command_line_async(
+                'flatpak override --user --filesystem=xdg-config/gtk-3.0:ro');
+        } catch (e) {
+            logError(e);
+        }
+        try {
+            GLib.spawn_command_line_async(
+                'flatpak override --user --filesystem=xdg-config/gtk-4.0:ro');
+        } catch(e) {
+            logError(e);
+        }
+    } else {
+          try {
+            GLib.spawn_command_line_async(
+                'flatpak override --user --nofilesystem=xdg-config/gtk-3.0');
+        } catch (e) {
+            logError(e);
+        }
+        try {
+            GLib.spawn_command_line_async(
+                'flatpak override --user --nofilesystem=xdg-config/gtk-4.0');
+        } catch(e) {
+            logError(e);
+        }
+    }     
+}
+
+function update_gtk3_theming(themeit, accentcolor) {
+    if (themeit == true) {
+        const theme = read_file(MeDir + '/resources/' + accentcolor + '/gtk.css');
+        write_file(theme, HomeDir + '/.config/gtk-3.0/gtk.css');
+    } else {
+        delete_file_dir(HomeDir + "/.config/gtk-3.0/gtk.css");
+    }  
+}
+
+function update_shell_theming(themeit, accentcolor) {
+    if (themeit == true) {
+        const theme = read_file(MeDir +
+            '/resources/' + accentcolor + '/gnome-shell/gnome-shell.css');
+        write_file(theme, HomeDir +
+            '/.local/share/themes/CustomAccentColors/gnome-shell/gnome-shell.css');
+    } else {
+        delete_file_dir(HomeDir +
+            '/.local/share/themes/CustomAccentColors/gnome-shell/gnome-shell.css');
+        delete_file_dir(HomeDir +
+            '/.local/share/themes/CustomAccentColors/gnome-shell/toggle-on.svg');
+        delete_file_dir(HomeDir +
+            '/.local/share/themes/CustomAccentColors/gnome-shell');
+        delete_file_dir(HomeDir + '/.local/share/themes/CustomAccentColors');
+    }
+}
---
+++ custom-accent-colors@demiskp/prefs.js
@@ -0,0 +1,153 @@
+/* prefs.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+const {Adw, Gio, GObject, Gtk} = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+
+class AccentColorSupportPrefsWidget extends Adw.PreferencesPage {
+    static {
+        GObject.registerClass(this);
+    }
+    constructor() {
+        super();
+
+        this._actionGroup = new Gio.SimpleActionGroup();
+
+        this._settings = ExtensionUtils.getSettings(
+            'org.gnome.shell.extensions.custom-accent-colors');
+
+        this._actionGroup.add_action(
+            this._settings.create_action('theme-flatpak'));
+        this._actionGroup.add_action(
+            this._settings.create_action('theme-gtk3'));
+        this._actionGroup.add_action(
+            this._settings.create_action('theme-shell'));
+
+        this.mainGroup = new Adw.PreferencesGroup();
+        this.add(this.mainGroup);
+        
+        const listModel = new Gio.ListStore({ item_type: ListAccentColor });
+        listModel.append(new ListAccentColor('Blue (Default)', 'blue'));
+        listModel.append(new ListAccentColor('Green', 'green'));
+        listModel.append(new ListAccentColor('Yellow', 'yellow'));
+        listModel.append(new ListAccentColor('Orange', 'orange'));
+        listModel.append(new ListAccentColor('Red', 'red'));
+        listModel.append(new ListAccentColor('Magenta', 'magenta'));
+        listModel.append(new ListAccentColor('Purple', 'purple'));
+        listModel.append(new ListAccentColor('Brown', 'brown'));
+        listModel.append(new ListAccentColor('Gray', 'gray'));
+        this.mainRow = new Adw.ComboRow({
+            title: 'Accent Color',
+            subtitle: 'Requires logout to activate properly.',
+            model: listModel,
+            expression: new Gtk.PropertyExpression(ListAccentColor, null, 'name'),
+        });
+        this.mainGroup.add(this.mainRow);
+        this.mainRow.connect('notify::selected-item', () => {
+            const { selectedItem } = this.mainRow;
+            this._settings.set_string('accent-color', selectedItem.value);
+        });
+        this._settings.connect('changed::color', () => {
+            this._updateSelectedColor();
+        });
+        this._updateSelectedColor();
+
+        this.extraGroup = new Adw.PreferencesGroup({
+            title: ('Extra Options'),
+        });
+        this.add(this.extraGroup);
+
+        let toggle = new Gtk.Switch({
+            action_name: 'theme-flatpak',
+            valign: Gtk.Align.CENTER,
+        });
+        this._settings.bind('theme-flatpak',
+            toggle, 'active', Gio.SettingsBindFlags.DEFAULT);
+        this.extraRow = new Adw.ActionRow({
+            title: ('Flatpak Theming'),
+            activatable_widget: toggle,
+        });
+        this.extraRow.add_suffix(toggle);
+        this.extraGroup.add(this.extraRow);
+
+        toggle = new Gtk.Switch({
+            action_name: 'theme-gtk3',
+            valign: Gtk.Align.CENTER,
+        });
+        this._settings.bind('theme-gtk3',
+            toggle, 'active', Gio.SettingsBindFlags.DEFAULT);
+        this.extraRow = new Adw.ActionRow({
+            title: ('GTK3 Theming'),
+            subtitle: ('Requires the "adw-gtk3" theme.'),
+            activatable_widget: toggle,
+        });
+        this.extraRow.add_suffix(toggle);
+        this.extraGroup.add(this.extraRow);
+        
+        toggle = new Gtk.Switch({
+            action_name: 'theme-shell',
+            valign: Gtk.Align.CENTER,
+        });
+        this._settings.bind('theme-shell',
+            toggle, 'active', Gio.SettingsBindFlags.DEFAULT);
+        this.extraRow = new Adw.ActionRow({
+            title: ('Shell Theming'),
+            subtitle: ('Requires the Shell Theme to be set to "AccentColorPicker".'),
+            activatable_widget: toggle,
+        });
+        this.extraRow.add_suffix(toggle);
+        this.extraGroup.add(this.extraRow);
+    }
+
+    _updateSelectedColor() {
+        const color = this._settings.get_string('accent-color');
+        const { model } = this.mainRow;
+        for (let i = 0; i < model.get_n_items(); i++) {
+            const item = model.get_item(i);
+            if (item.value === color) {
+                this.mainRow.set_selected(i);
+                break;
+            }
+        }
+    }
+}
+
+const ListAccentColor = GObject.registerClass({
+      Properties: {
+          'name': GObject.ParamSpec.string(
+              'name', 'name', 'name',
+              GObject.ParamFlags.READWRITE,
+              null),
+          'value': GObject.ParamSpec.string(
+              'value', 'value', 'value',
+              GObject.ParamFlags.READWRITE,
+              null),
+      },
+}, class ListAccentColor extends GObject.Object {
+      _init(name, value) {
+          super._init({ name, value });
+      }
+});
+
+function init() {
+}
+
+function buildPrefsWidget() {
+    return new AccentColorSupportPrefsWidget();
+}
